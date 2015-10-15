88e65ccb9f7dc64e22f93ee0fb1b00a9a0c06ca8
Léo POIROUX <leo.poiroux@gmail.com>
2015-10-15 14:11:27 +0200

    Don't install source by default

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,81 +6,91 @@
 #  apt
 # Sample Usage:
 #  include java8
-class java8 {
-  case $::operatingsystem {
-    debian, ubuntu: {
-      file { '/tmp/java.preseed':
-        source => 'puppet:///modules/java8/java.preseed',
-        mode   => '0600',
-        backup => false,
-      }
+class java8(
+$repo = false,
+) {
+
+    case $::operatingsystem {
+        debian, ubuntu: {
+            file { '/tmp/java.preseed':
+                source => 'puppet:///modules/java8/java.preseed',
+                mode   => '0600',
+                backup => false,
+            }
+        }
+        default: { notice "Unsupported operatingsystem ${::operatingsystem}" }
     }
 
-    default: { notice "Unsupported operatingsystem ${::operatingsystem}" }
-  }
+    case $::operatingsystem {
+        debian: {
+            if $repo {
+                include apt
+                apt::source { 'webupd8team-java':
+                    location    => 'http://ppa.launchpad.net/webupd8team/java/ubuntu',
+                    release     => 'precise',
+                    repos       => 'main',
+                    key         => '7B2C3B0889BF5709A105D03AC2518248EEA14886',
+                    key_server  => 'keyserver.ubuntu.com',
+                    include_src => true
+                }
 
-  case $::operatingsystem {
-    debian: {
-      include apt
+                exec { 'apt-update-post-webupd8team-apt-source':
+                    command => "/usr/bin/apt-get update",
+                    require => Apt::Source['webupd8team-java']
+                }
 
-      apt::source { 'webupd8team-java':
-        location    => 'http://ppa.launchpad.net/webupd8team/java/ubuntu',
-        release     => 'precise',
-        repos       => 'main',
-        key         => '7B2C3B0889BF5709A105D03AC2518248EEA14886',
-        key_server  => 'keyserver.ubuntu.com',
-        include_src => true
-      }
+                package { 'oracle-java8-installer':
+                    responsefile => '/tmp/java.preseed',
+                    require      => [
+                        Apt::Source['webupd8team-java'],
+                        Exec['apt-update-post-webupd8team-apt-source'],
+                        File['/tmp/java.preseed']
+                    ],
+                }
+            } else {
+                package { 'oracle-java8-installer':
+                    responsefile => '/tmp/java.preseed',
+                    require      => File['/tmp/java.preseed']
+                }
+            }
+        }
 
-      exec { 'apt-update-post-webupd8team-apt-source':
-        command => "/usr/bin/apt-get update",
-        require      => [
-          Apt::Source['webupd8team-java']
-        ],
-       }
+        ubuntu: {
+            if $repo {
+                include apt
+                apt::ppa { 'ppa:webupd8team/java': }
 
-      package { 'oracle-java8-installer':
-        responsefile => '/tmp/java.preseed',
-        require      => [
-          Apt::Source['webupd8team-java'],
-          Exec['apt-update-post-webupd8team-apt-source'],
-          File['/tmp/java.preseed']
-        ],
-      }
+                exec { 'apt-update':
+                    command => "/usr/bin/apt-get update",
+                    require => Apt::Ppa['ppa:webupd8team/java']
+                }
+
+                package { 'oracle-java8-installer':
+                    responsefile => '/tmp/java.preseed',
+                    require      => [
+                        Exec['apt-update'],
+                        File['/tmp/java.preseed']
+                    ],
+                }
+            } else {
+                package { 'oracle-java8-installer':
+                    responsefile => '/tmp/java.preseed',
+                    require      => File['/tmp/java.preseed']
+                }
+            }
+        }
+
+        default: {
+            notice "Unsupported operatingsystem ${::operatingsystem}"
+        }
     }
 
-    ubuntu: {
-      include apt
-
-      apt::ppa { 'ppa:webupd8team/java': }
-
-      exec { 'apt-update':
-        command => "/usr/bin/apt-get update",
-        require      => [
-          Apt::Ppa['ppa:webupd8team/java']
-        ],
-       }
-
-      package { 'oracle-java8-installer':
-        responsefile => '/tmp/java.preseed',
-        require      => [
-          Exec['apt-update'],
-          File['/tmp/java.preseed']
-        ],
-       }
+    file { '/etc/profile.d/set_java_home.sh':
+        ensure  => file,
+        group   => root,
+        owner   => root,
+        mode    => '0755',
+        source  => 'puppet:///modules/java8/set_java_home.sh',
+        require => Package['oracle-java8-installer']
     }
-
-    default: {
-      notice "Unsupported operatingsystem ${::operatingsystem}"
-    }
-  }
-
-  file { '/etc/profile.d/set_java_home.sh':
-    ensure  => file,
-    group   => root,
-    owner   => root,
-    mode    => '0755',
-    source  => 'puppet:///modules/java8/set_java_home.sh',
-    require => Package['oracle-java8-installer']
-  }
 }
